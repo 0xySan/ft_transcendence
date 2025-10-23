8d6319bab44738b731731d061afacb75a77218aa
Reynald Leroy <releroy@c1r4p3.42lehavre.fr>
2025-10-23 13:30:55 +0200

    Add the TailwindCSS variables for the Catppuccin colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#1e40af", // bleu foncé
-        secondary: "#9333ea", // violet
-        accent: "#f59e0b" // orange
+        rosewater: "rgb(var(--rosewater) / <alpha-value>)",
+        flamingo: "rgb(var(--flamingo) / <alpha-value>)",
+        pink: "rgb(var(--pink) / <alpha-value>)",
+        mauve: "rgb(var(--mauve) / <alpha-value>)",
+        red: "rgb(var(--red) / <alpha-value>)",
+        maroon: "rgb(var(--maroon) / <alpha-value>)",
+        peach: "rgb(var(--peach) / <alpha-value>)",
+        yellow: "rgb(var(--yellow) / <alpha-value>)",
+        green: "rgb(var(--green) / <alpha-value>)",
+        teal: "rgb(var(--teal) / <alpha-value>)",
+        sky: "rgb(var(--sky) / <alpha-value>)",
+        sapphire: "rgb(var(--sapphire) / <alpha-value>)",
+        blue: "rgb(var(--blue) / <alpha-value>)",
+        lavender: "rgb(var(--lavender) / <alpha-value>)",
+        text: "rgb(var(--text) / <alpha-value>)",
+        subtext1: "rgb(var(--subtext1) / <alpha-value>)",
+        subtext0: "rgb(var(--subtext0) / <alpha-value>)",
+        overlay2: "rgb(var(--overlay2) / <alpha-value>)",
+        overlay1: "rgb(var(--overlay1) / <alpha-value>)",
+        overlay0: "rgb(var(--overlay0) / <alpha-value>)",
+        base: "rgb(var(--base) / <alpha-value>)",
+        mantle: "rgb(var(--mantle) / <alpha-value>)",
+        crust: "rgb(var(--crust) / <alpha-value>)"
       },
       fontFamily: {
         sans: ["Inter", "ui-sans-serif", "system-ui"]
