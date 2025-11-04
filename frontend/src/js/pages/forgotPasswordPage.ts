428574c8f98e4ddfaaa8050f8a8a5f21b8f422e1
Rey <reynald-leroy@hotmail.fr>
2025-11-04 18:03:53 +0100

    Add new TS scripts for auth pages + add twoFA page

---
+++ frontend/src/js/pages/forgotPasswordPage.ts
@@ -0,0 +1,44 @@
+export {};
+
+declare function addListener(target: EventTarget | null, event: string, handler: EventListenerOrEventListenerObject): void;
+
+const forgotPasswordForm = document.querySelector<HTMLFormElement>('.auth-form-container');
+const emailInput = document.getElementById('email-text-input') as HTMLInputElement | null;
+const resetButton = document.querySelector<HTMLButtonElement>('button[type="submit"]');
+
+function handleForgotPassword(event: Event): void {
+  event.preventDefault();
+  const isEmailValid = verifyEmailValidity();
+
+  if (isEmailValid) {
+    console.log('Form is valid. Submitting...');
+    // forgotPasswordForm?.submit();
+    // or: forgotPasswordForm?.requestSubmit?.();
+  } else {
+    console.log('Form is invalid. Please correct the errors and try again.');
+  }
+}
+
+function verifyEmailValidity(): boolean {
+  const emailRegex: RegExp = /^[\p{L}\p{N}._%+-]{1,64}@[A-Za-z0-9.-]{1,255}\.[A-Za-z]{2,}$/u;
+  const email: string = emailInput?.value ?? '';
+  const errorTextElement: HTMLElement | null = document.getElementById('forgot-password-email-error-text');
+
+  if (!errorTextElement) return false;
+
+  if (!emailRegex.test(email)) {
+    errorTextElement.textContent = 'Please enter a valid email address.';
+    errorTextElement.classList.add('visible');
+    errorTextElement.setAttribute('aria-hidden', 'false');
+    console.log('Invalid email:', email);
+    return false;
+  } else {
+    errorTextElement.textContent = '';
+    errorTextElement.classList.remove('visible');
+    errorTextElement.setAttribute('aria-hidden', 'true');
+    return true;
+  }
+}
+
+addListener(resetButton, 'click', handleForgotPassword);
+addListener(emailInput, 'input', verifyEmailValidity);
