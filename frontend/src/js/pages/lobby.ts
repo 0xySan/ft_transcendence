7d1b712d36bff3466406c97dd9d90eaee10c2be2
Rey <reynald-leroy@hotmail.fr>
2025-11-20 16:42:37 +0100

    Style corrections on buttons and more listeners created for elements of the lobby page

--- frontend/src/js/pages/lobby.ts
+++ frontend/src/js/pages/lobby.ts
@@ -5,9 +5,22 @@ declare function addListener(target: EventTarget | null, event: string, handler:
 const multiplayerButton = document.getElementById("lobby-multiplayer-button") as HTMLButtonElement | null;
 const customGameButton = document.getElementById("lobby-custom-game-button") as HTMLButtonElement | null;
 const tournamentButton = document.getElementById("lobby-tournament-button") as HTMLButtonElement | null;
+
 const multiplayerTab = document.getElementById("lobby-multiplayer-tab") as HTMLDivElement | null;
 const customGameTab = document.getElementById("lobby-custom-game-tab") as HTMLDivElement | null;
 const tournamentTab = document.getElementById("lobby-tournament-tab") as HTMLDivElement | null;
+
+const customGameBasicSettingButton = document.getElementById("lobby-custom-game-basic-settings-button") as HTMLButtonElement | null;
+const customGameAdvancedSettingButton = document.getElementById("lobby-custom-game-advanced-settings-button") as HTMLButtonElement | null;
+
+const tournamentBasicSettingButton = document.getElementById("lobby-tournament-basic-settings-button") as HTMLButtonElement | null;
+const tournamentAdvancedSettingButton = document.getElementById("lobby-tournament-advanced-settings-button") as HTMLButtonElement | null;
+
+const customGameBasicSettingsTab = document.getElementById("lobby-custom-game-basic-settings") as HTMLDivElement | null;
+const customGameAdvancedSettingsTab = document.getElementById("lobby-custom-game-advanced-settings") as HTMLDivElement | null;
+
+const tournamentBasicSettingsTab = document.getElementById("lobby-tournament-basic-settings") as HTMLDivElement | null;
+const tournamentAdvancedSettingsTab = document.getElementById("lobby-tournament-advanced-settings") as HTMLDivElement | null;
 
 function deactivateOtherButtonsThan(button: HTMLButtonElement | null) {
     if (!button) return;
@@ -58,7 +71,54 @@ function handleTournamentButtonClick(evt: Event) {
     showCorrectTab(tournamentTab);
 }
 
+function handleCustomGameBasicSettingsButtonClick(evt: Event) {
+    const button = evt.currentTarget as HTMLButtonElement | null;
+    if (!button) return;
+    if (button !== customGameBasicSettingButton) return;
+    if (button.classList.contains("selected")) return;
+    button.classList.add("selected");
+    customGameAdvancedSettingButton?.classList.remove("selected");
+    customGameAdvancedSettingsTab?.classList.add("unloaded");
+    customGameBasicSettingsTab?.classList.remove("unloaded");
+}
+
+function handleCustomGameAdvancedSettingsButtonClick(evt: Event) {
+    const button = evt.currentTarget as HTMLButtonElement | null;
+    if (!button) return;
+    if (button !== customGameAdvancedSettingButton) return;
+    if (button.classList.contains("selected")) return;
+    button.classList.add("selected");
+    customGameBasicSettingButton?.classList.remove("selected");
+    customGameBasicSettingsTab?.classList.add("unloaded");
+    customGameAdvancedSettingsTab?.classList.remove("unloaded");
+}
+
+function handleTournamentBasicSettingsButtonClick(evt: Event) {
+    const button = evt.currentTarget as HTMLButtonElement | null;
+    if (!button) return;
+    if (button !== tournamentBasicSettingButton) return;
+    if (button.classList.contains("selected")) return;
+    button.classList.add("selected");
+    tournamentAdvancedSettingButton?.classList.remove("selected");
+    tournamentAdvancedSettingsTab?.classList.add("unloaded");
+    tournamentBasicSettingsTab?.classList.remove("unloaded");
+}
+
+function handleTournamentAdvancedSettingsButtonClick(evt: Event) {
+    const button = evt.currentTarget as HTMLButtonElement | null;
+    if (!button) return;
+    if (button !== tournamentAdvancedSettingButton) return;
+    if (button.classList.contains("selected")) return;
+    button.classList.add("selected");
+    tournamentBasicSettingButton?.classList.remove("selected");
+    tournamentBasicSettingsTab?.classList.add("unloaded");
+    tournamentAdvancedSettingsTab?.classList.remove("unloaded");
+}
+
 addListener(multiplayerButton, 'click', handleMultiplayerButtonClick);
 addListener(customGameButton, 'click', handleCustomGameButtonClick);
 addListener(tournamentButton, 'click', handleTournamentButtonClick);
-console.log(multiplayerTab, customGameTab, tournamentTab);
+addListener(customGameBasicSettingButton, 'click', handleCustomGameBasicSettingsButtonClick);
+addListener(customGameAdvancedSettingButton, 'click', handleCustomGameAdvancedSettingsButtonClick);
+addListener(tournamentBasicSettingButton, 'click', handleTournamentBasicSettingsButtonClick);
+addListener(tournamentAdvancedSettingButton, 'click', handleTournamentAdvancedSettingsButtonClick);
