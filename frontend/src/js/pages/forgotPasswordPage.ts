36982118bb5816bc651d4c87cc1d8292f602ac9e
Moutig <moutig@moutig.sh>
2025-11-25 12:04:28 +0000

    Refactor(css): Improved forgot-password page responsivness

--- frontend/src/js/pages/forgotPasswordPage.ts
+++ frontend/src/js/pages/forgotPasswordPage.ts
@@ -7,37 +7,37 @@ const emailInput = document.getElementById('email-text-input') as HTMLInputEleme
 const resetButton = document.querySelector<HTMLButtonElement>('button[type="submit"]');
 
 function handleForgotPassword(event: Event): void {
-  event.preventDefault();
-  const isEmailValid = verifyEmailValidity();
+	event.preventDefault();
+	const isEmailValid = verifyEmailValidity();
 
-  if (isEmailValid) {
-    console.log('Form is valid. Submitting...');
-    // forgotPasswordForm?.submit();
-    // or: forgotPasswordForm?.requestSubmit?.();
-  } else {
-    console.log('Form is invalid. Please correct the errors and try again.');
-  }
+	if (isEmailValid) {
+		console.log('Form is valid. Submitting...');
+		// forgotPasswordForm?.submit();
+		// or: forgotPasswordForm?.requestSubmit?.();
+	} else {
+		console.log('Form is invalid. Please correct the errors and try again.');
+	}
 }
 
 function verifyEmailValidity(): boolean {
-  const emailRegex: RegExp = /^[\p{L}\p{N}._%+-]{1,64}@[A-Za-z0-9.-]{1,255}\.[A-Za-z]{2,}$/u;
-  const email: string = emailInput?.value ?? '';
-  const errorTextElement: HTMLElement | null = document.getElementById('forgot-password-email-error-text');
+	const emailRegex: RegExp = /^[\p{L}\p{N}._%+-]{1,64}@[A-Za-z0-9.-]{1,255}\.[A-Za-z]{2,}$/u;
+	const email: string = emailInput?.value ?? '';
+	const errorTextElement: HTMLElement | null = document.getElementById('forgot-password-email-error-text');
 
-  if (!errorTextElement) return false;
+	if (!errorTextElement) return (false);
 
-  if (!emailRegex.test(email)) {
-    errorTextElement.textContent = 'Please enter a valid email address.';
-    errorTextElement.classList.remove('hidden');
-    errorTextElement.setAttribute('aria-hidden', 'false');
-    console.log('Invalid email:', email);
-    return false;
-  } else {
-    errorTextElement.textContent = '';
-    errorTextElement.classList.add('hidden');
-    errorTextElement.setAttribute('aria-hidden', 'true');
-    return true;
-  }
+	if (!emailRegex.test(email)) {
+		errorTextElement.textContent = 'Please enter a valid email address.';
+		errorTextElement.setAttribute('aria-hidden', 'false');
+		errorTextElement.style.opacity = '1';
+		console.log('Invalid email:', email);
+		return (false);
+	} else {
+		errorTextElement.textContent = '';
+		errorTextElement.setAttribute('aria-hidden', 'true');
+		errorTextElement.style.opacity = '0';
+		return (true);
+	}
 }
 
 addListener(resetButton, 'click', handleForgotPassword);
